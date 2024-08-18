b8f32faaf517ff0be1c06f0864c08c148494f25e
Saba <alireza_mac@Sabas-Mac.local>
2024-08-18 16:38:56 +0330

    added

---
+++ assets/javascript/nav.js
@@ -0,0 +1,5 @@
+function myMenu() {
+    let currentMenu = document.getElementsByClassName(nav_1)
+
+    if (currentMenu.style.display == "block") { currentMenu.style.display = "none"; } else { currentMenu.style.display = "block"; }
+}
